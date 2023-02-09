2ed70817da4f6e4aa3869906c90aebd410a8a2c1
Женя Шульман <jonnie.gray@yandex.ru>
2023-02-09 16:27:14 +0500

    Review#4.1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Group, User, Follow
 from .forms import PostForm, CommentForm
